5bcd26c72de6dcab1abd91d63755c36112c62059
Jmcus <vluxyue@gmail.com>
2015-04-17 19:47:53 +0800

    js

---
+++ Array_Remove.js
@@ -0,0 +1,5 @@
+Array.remove = function(array,from,to){
+    var rest = array.slice((to || from) + 1 || array.length);
+    array.length = from < 0 ? array.length + from : from;
+    return array.push.apply(apply,rest);
+};
